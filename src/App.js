d29a8c2730688c1481e576a1e669b3520982ad2c
Wamaitha <wamaithaweru19@gmail.com>
2022-08-30 14:42:39 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect } from 'react';
+
+const API_URL="http://www.omdbapi.com/?i=tt3896198&apikey=a135fa27"
 
 function App() {
+  const searchMovies = async(title) => {
+    const response  = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+  }
+
+  useEffect(() => {
+
+  }, []);
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
